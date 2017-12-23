c6a3302cee1a098d8ccfe21c286b28d99e2f0376
don <don.tinimbang@gmail.com>
2017-12-22 23:47:53 -0600

    game tested

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,22 @@ var guess =0;
 
 
 var startGame = function() {
-    $(".crystals").empty();
+    $(".crystals").empty(); //empty the crystal
 
-    random_result = Math.floor(Math.random() * 69) + 30; // hoisting
-    console.log(random_result);
+    var image =[]
+    [0]= 
+
+    random_result = Math.floor(Math.random() * 69) + 30; // generate new result
+    console.log(random_result); // log new number
     
     $("#result").html('Random Result:  ' + random_result);
-    for (var i = 0; i<4; i++){
+    for (var i = 0; i<4; i++){ // loop 4 times and every time create random number
     
         var random = Math.floor(Math.random() * 11) +1; // randomizes no.
        // console.log(random);
     
-        var crystal = $("<div>");
-        crystal.attr({
+        var crystal = $("<div>"); // creat the div
+        crystal.attr({          //add attribute to class
             "class": 'crystal',
             "data-random": random  // game with 4 crystals and a random result
         });
@@ -29,13 +32,14 @@ var startGame = function() {
 
 }
 
-startGame();
+startGame(); //start game
 
 var reset = function(){
 
 
 }
-//event delegation?
+//event delegation
+// when page loads it will find everything to work with
 $(document).on('click', ".crystal", function() {
 
 
@@ -45,9 +49,10 @@ $(document).on('click', ".crystal", function() {
    var result = num + 5;
 
     guess += num;
+    $("#guess").html("guess:  "  + guess);
     console.log(guess);
     if(guess > random_result){
-        lost--;
+        lost++;
         $("#lost").html("lose: " + lost);
         startGame();
         guess = 0;
