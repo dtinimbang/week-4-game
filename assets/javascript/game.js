b7b42a41f610e56bcd060bd5bcbfa41cf12b2d4e
don <don.tinimbang@gmail.com>
2017-12-22 20:15:38 -0600

    random number working yaaay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,50 +2,21 @@ var random_result;
 var lost;
 var win;
 
+
 random_result = Math.floor(Math.random() * 69) + 30; // hoisting
+console.log(random_result);
 
-function scoreSum(sumTotal) {
-    //yourNumber+=sumTotal
-    yourNumber = yourNumber + sumTotal;
-    console.log("", yourNumber);
-  }
+$("#result").html('Random Result:  ');
+for (var i = 0; i<4; i++){
 
-$("#result").html('random result: ' + random_result);
+    var random = Math.floor(Math.random() * 11) +1; // randomizes no.
+   // console.log(random);
 
-
-    for(var i = 0; i<4; i++){
-         
-        var random = Math.floor(Math.random() * 11) +1;
-        console.log(random);
-
-
-         var crystal = $("<div>");
-         crystal.attr({
-             "class": 'crystal',
-             "data-random": random 
-
-        
-             
-         });
-         
-         $(".crystals").append(crystal);
-    }
-
-    $("#red").on('click', function(){
-        var num = parseInt($(crystal).attr('data-random'));
-        console.log(num);
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": random  // game with 4 crystals and a random result
     });
 
-    
-
-    $("#pink").on('click', function(){
-        console.log($(crystal).attr('data-random'));
-    });
-
-    $("#purple").on('click', function(){
-        console.log($(this));
-    });
-
-    $("#green").on('click', function(){
-        console.log($(this));
-    });
+    $(".crystals").append(crystal);
+}
