154843d14e41122254c539fa1dbbae87e68a10d2
don <don.tinimbang@gmail.com>
2017-12-22 23:25:35 -0600

    set guess to 0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,39 @@ var win=0;
 var guess =0;
 
 
+var startGame = function() {
+    $(".crystals").empty();
 
+    random_result = Math.floor(Math.random() * 69) + 30; // hoisting
+    console.log(random_result);
+    
+    $("#result").html('Random Result:  ' + random_result);
+    for (var i = 0; i<4; i++){
+    
+        var random = Math.floor(Math.random() * 11) +1; // randomizes no.
+       // console.log(random);
+    
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random  // game with 4 crystals and a random result
+        });
+    
+       
+    
+        $(".crystals").append(crystal);
+    }
 
-
-random_result = Math.floor(Math.random() * 69) + 30; // hoisting
-console.log(random_result);
-
-$("#result").html('Random Result:  ' + random_result);
-for (var i = 0; i<4; i++){
-
-    var random = Math.floor(Math.random() * 11) +1; // randomizes no.
-   // console.log(random);
-
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystal',
-        "data-random": random  // game with 4 crystals and a random result
-    });
-
-   
-
-    $(".crystals").append(crystal);
 }
 
-$(".crystal").on('click', function() {
+startGame();
+
+var reset = function(){
+
+
+}
+//event delegation?
+$(document).on('click', ".crystal", function() {
 
 
 
@@ -39,12 +48,16 @@ $(".crystal").on('click', function() {
     console.log(guess);
     if(guess > random_result){
         lost--;
-        $("#lost").html("lose: " + win);
+        $("#lost").html("lose: " + lost);
+        startGame();
+        guess = 0;
         console.log("loser");
     }
     else if (guess === random_result){
         win++
         $("#win").html("win: " + win);
+        startGame();
+        guess = 0;
         console.log('winner');
     }
 
